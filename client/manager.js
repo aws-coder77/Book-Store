8f0cd398592485c6d32a30c29e1bcc17afc1ba77
Gulshan Anand <gulshan2052@gmail.com>
2023-08-21 15:28:02 +0530

    manager page done

--- client/manager.js
+++ client/manager.js
@@ -37,34 +37,66 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // Your code to run since DOM is loaded and ready
 });
 
-function addBook() {
+google.charts.load('current', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+// google.charts.setOnLoadCallback(drawChart);
+
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+        ['Mushrooms', 3],
+        ['Onions', 1],
+        ['Olives', 1],
+        ['Zucchini', 1],
+        ['Pepperoni', 2]
+    ]);
+
+    // Set chart options
+    var options = {'title':'How Much Pizza I Ate Last Night',
+                    'width':400,
+                    'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
+
+function getBookComp() {
     return `
         <div class="height-100 bg-light">
             <h4>Add Book</h4>
             <form>
                 <div class="form-group">
                     <label for="formGroupExampleInput">Book Title</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Book Title">
+                    <input type="text" class="form-control" name="title" placeholder="Book Title">
                 </div>
                 <div class="form-group">
                     <label for="formGroupExampleInput2">Author</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Author">
+                    <input type="text" class="form-control" name="author" placeholder="Author">
                 </div>
                 <div class="form-group">
                     <label for="formGroupExampleInput2">Publication</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Publication">
+                    <input type="text" class="form-control" name="pub" placeholder="Publication">
                 </div>
                 <div class="form-group">
                     <label for="formGroupExampleInput2">MRP</label>
-                    <input type="number" class="form-control" id="formGroupExampleInput2" placeholder="MRP">
+                    <input type="number" class="form-control" name="mrp" placeholder="MRP">
                 </div>
                 <div class="form-group">
                     <label for="formGroupExampleInput2">Price</label>
-                    <input type="number" class="form-control" id="formGroupExampleInput2" placeholder="Price">
+                    <input type="number" class="form-control" name="price" placeholder="Price">
                 </div>
                 <div class="form-group">
                     <label for="formGroupExampleInput2">Quantity</label>
-                    <input type="number" class="form-control" id="formGroupExampleInput2" placeholder="Quantity">
+                    <input type="number" class="form-control" name="quantity" placeholder="Quantity">
                 </div>
                 <div class="form-group divcenter">
                     <input class="btn btn-primary btn-submit" type="submit" value="Submit">
@@ -74,36 +106,42 @@ function addBook() {
     `;
 }
 
-function addStaff() {
+function getStaffComp() {
     return `
         <div class="height-100 bg-light">
             <h4>Add Staff</h4>
             <form>
                 <div class="form-group">
                     <label for="formGroupExampleInput">Name</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Name">
+                    <input type="text" class="form-control" name="name" placeholder="Name">
                 </div>
                 <div class="form-group">
                     <label for="formGroupExampleInput2">Email</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Email">
+                    <input type="text" class="form-control" name="email" placeholder="Email">
                 </div>
                 <div class="form-group">
                     <label for="formGroupExampleInput2">Phone Number</label>
-                    <input type="number" class="form-control" id="formGroupExampleInput2" placeholder="Phone Number">
+                    <input type="number" class="form-control" name="phone" placeholder="Phone Number">
                 </div>
                 <div class="form-group">
                     <label for="formGroupExampleInput2">Password</label>
-                    <input type="password" class="form-control" id="formGroupExampleInput2" placeholder="Password">
+                    <input type="password" class="form-control" name="passeord" placeholder="Password">
                 </div>
                 <div class="form-group">
                     <label for="formGroupExampleInput2">Address</label>
-                    <textarea type="text" class="form-control txtarea" id="formGroupExampleInput2" placeholder="Address"></textarea>
+                    <textarea type="text" class="form-control txtarea" name="address" placeholder="Address"></textarea>
                 </div>
                 <div class="form-group divcenter">
                     <input class="btn btn-primary btn-submit" type="submit" value="Submit">
                 </div>
             </form>
         </div>
+    `;
+}
+
+function getStatsComp() {
+    return `
+        <div id="chart_div"></div>
     `;
 }
 
@@ -114,15 +152,16 @@ const statsDiv = document.getElementById("stats");
 
 addBookDiv.addEventListener("click", function (event) {
     event.preventDefault();
-    contentDiv.innerHTML = addBook();
+    contentDiv.innerHTML = getBookComp();
 });
 
 addStaffDiv.addEventListener("click", function (event) {
     event.preventDefault();
-    contentDiv.innerHTML = addStaff();
+    contentDiv.innerHTML = getStaffComp();
 });
 
-statsDiv    .addEventListener("click", function (event) {
+statsDiv.addEventListener("click", function (event) {
     event.preventDefault();
-    contentDiv.innerHTML = "<p>This is the content for Link 3.</p>";
+    contentDiv.innerHTML = getStatsComp();
+    drawChart();
 });
